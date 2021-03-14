ff89d36d78157229ef54221ddf5fe52d7966cd3e
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-03-14 16:42:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var mallText = document.querySelector("#mallSoftText").textContent;
 mallText = "Happiness";
 mallSoftText.style.color = "teal";
-
+page1Btn.style.background = "purple";
 var page1Btn = document.getElementById("btnPage1");
 page1Btn.addEventListener("Click", function() {
     console.log("Call 1-800-tRopicAL-PoPicAl");
 })
-page1Btn.style.background = "purple";
+
 
 
