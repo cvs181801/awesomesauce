398d374c9665a47343b4625cc04d3e4fb8caf058
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-03-14 16:40:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var page1Btn = document.getElementById("btnPage1");
 page1Btn.addEventListener("Click", function() {
     console.log("Call 1-800-tRopicAL-PoPicAl");
 })
-page1Btn.style.background = "violet";
+page1Btn.style.background = "purple";
 
 
