05f26cd9a3e25cf533ea85dda16cf1a3c7719947
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-03-16 17:32:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var mallText = document.querySelector("#mallSoftText").textContent;
 mallText = "Happiness";
 mallSoftText.style.color = "teal";
+
+//add button controls
 page1Btn.style.background = "purple";
 var page1Btn = document.getElementById("btnPage1");
-page1Btn.addEventListener("Click", function() {
-    console.log("Call 1-800-tRopicAL-PoPicAl");
-})
 
+var handleButtonClick() {
+    console.log("Call 1-800-tRopicAL-PoPicAl");
+}
+page1Btn.addEventListener("Click", handleButtonClick);
+
+//add text box controls
 var inputBox = document.getElementById("text-boxPg1")
 inputBox.value = "Hey!";
+
 
 
 
